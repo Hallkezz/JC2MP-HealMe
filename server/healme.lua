f89b796b3dc6a2f457c3bd5337d8d8387a061bb8
Hallkezz <32846474+Hallkezz@users.noreply.github.com>
2025-07-11 20:51:19 +0300

    Update to version 0.2

--- server/healme.lua
+++ server/healme.lua
@@ -4,15 +4,14 @@
 
 -----------------------------------------------------------------------------------
 --Settings
-local Active = "/heal" -- Heal Activate command.
-local Active2 = "/healme" -- Heal Activate command 2.
+local command = "/heal" -- Command to heal.
 ---------------------
-local Warning = "You can not use it here!" -- Warning Text.
-local WarningColor = Color.Red -- Warning Color.
-local DoneText = "You are healed." -- Done Text.
-local DoneColor = Color.Chartreuse -- Done Color.
+local warning = "You can not use it here!" -- Warning text
+local warningColor = Color.Red -- Warning color
+local doneText = "You are healed." -- Done text
+local doneColor = Color.Chartreuse -- Done color
 ---------------------
-local Health = 1 -- Health LVL. (Use: 0.1 - 1)
+local health = 1 -- Health level (Use: 0.1 - 1)
 -----------------------------------------------------------------------------------
 
 -----------------------------------------------------------------------------------
@@ -20,27 +19,27 @@ local Health = 1 -- Health LVL. (Use: 0.1 - 1)
 class 'HealMe'
 
 function HealMe:__init()
-	Events:Subscribe( "PlayerChat", self, self.PlayerChat )
+    Events:Subscribe("PlayerChat", self, self.PlayerChat)
 end
 
-function HealMe:PlayerChat( args )
-    local cmd_args = args.text:split( " " )
+function HealMe:PlayerChat(args)
+    local cmd_args = args.text:split(" ")
 
-    if cmd_args[1] == Active or cmd_args[1] == Active2 then
-		if args.player:GetWorld() ~= DefaultWorld then
-			Chat:Send(args.player, Warning, WarningColor )
-			return
-		end		
-		args.player:SetHealth(Health)
-		Chat:Send(args.player, DoneText, DoneColor)
+    if cmd_args[1] == command then
+        if args.player:GetWorld() ~= DefaultWorld then
+            Chat:Send(args.player, warning, warningColor)
+            return
+        end
+
+        args.player:SetHealth(args.player:GetHealth() + health)
+        Chat:Send(args.player, doneText, doneColor)
+
         return false
     end
-
-    return true
 end
 
-healme = HealMe()
+local healme = HealMe()
 -----------------------------------------------------------------------------------
 
---v0.1--
---03.17.18--
+--v0.2--
+--11.07.25--
